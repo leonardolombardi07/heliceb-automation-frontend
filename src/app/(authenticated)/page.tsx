966c9cc109f4ada1046487016ca5f208f25894d2
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2024-06-26 04:08:03 +0000

    put design parameters first

--- src/app/(authenticated)/page.tsx
+++ src/app/(authenticated)/page.tsx
@@ -89,175 +89,6 @@ function PageContent() {
         <SectionTitle id={TOC[0].hash}>{TOC[0].text}</SectionTitle>
         <Box>
           <Typography>
-            As variáveis de ambiente configuram as condições físicas nas quais o
-            propulsor deve operar.
-          </Typography>
-
-          <Box sx={{ mt: 4 }} />
-
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(ρ) Densidade da Água do Mar"
-                value={getValue(formValues["seaWaterDensity"])}
-                onChange={(event) => {
-                  onChangeValue(Number(event.target.value), "seaWaterDensity");
-                }}
-                unit={getUnit(formValues["seaWaterDensity"])}
-                units={["kg/m³"]}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(g) Aceleração da Gravidade"
-                value={getValue(formValues["gravityAcceleration"])}
-                onChange={(event) => {
-                  onChangeValue(
-                    Number(event.target.value),
-                    "gravityAcceleration"
-                  );
-                }}
-                unit={getUnit(formValues["gravityAcceleration"])}
-                units={["m/s²"]}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(Pa) Pressão Atmosférica"
-                value={getValue(formValues["atmosphericPressure"])}
-                onChange={(event) => {
-                  onChangeValue(
-                    Number(event.target.value),
-                    "atmosphericPressure"
-                  );
-                }}
-                unit={getUnit(formValues["atmosphericPressure"])}
-                units={["Pa"]}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(Ps) Pressão de Saturação da Água do Mar"
-                value={getValue(formValues["saturationPressure"])}
-                onChange={(event) => {
-                  onChangeValue(
-                    Number(event.target.value),
-                    "saturationPressure"
-                  );
-                }}
-                unit={getUnit(formValues["saturationPressure"])}
-                units={["Pa"]}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(v) Viscosidade Cinemática da Água do Mar"
-                value={getValue(formValues["kinematicViscosity"])}
-                onChange={(event) => {
-                  onChangeValue(
-                    Number(event.target.value),
-                    "kinematicViscosity"
-                  );
-                }}
-                unit={getUnit(formValues["kinematicViscosity"])}
-                units={["m²/s"]}
-              />
-            </Grid>
-          </Grid>
-        </Box>
-      </SectionContainer>
-
-      <SectionContainer>
-        <SectionTitle id={TOC[1].hash}>{TOC[1].text}</SectionTitle>
-        <Box>
-          <Typography>
-            As restrições são as condições que o sistema deve atender para
-            satisfazer os requisitos do projeto.
-          </Typography>
-
-          <Box sx={{ mt: 4 }} />
-
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(n) Máximo Número de Propulsores"
-                value={getValue(formValues["maxNumberOfResults"])}
-                onChange={(event) => {
-                  onChangeValue(
-                    Number(event.target.value),
-                    "maxNumberOfResults"
-                  );
-                }}
-                unit={getUnit(formValues["maxNumberOfResults"])}
-                units={["-"]}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(nr) Eficiência Mínima"
-                value={getValue(formValues["minEfficiency"])}
-                onChange={(event) => {
-                  onChangeValue(Number(event.target.value), "minEfficiency");
-                }}
-                unit={getUnit(formValues["minEfficiency"])}
-                units={["-"]}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(T min) Empuxo Entregue Mínimo"
-                value={getValue(formValues["minDeliveredThrust"])}
-                onChange={(event) => {
-                  onChangeValue(
-                    Number(event.target.value),
-                    "minDeliveredThrust"
-                  );
-                }}
-                unit={getUnit(formValues["minDeliveredThrust"])}
-                units={["kN"]}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(T max) Empuxo Entregue Máximo"
-                value={getValue(formValues["maxDeliveredThrust"])}
-                onChange={(event) => {
-                  onChangeValue(
-                    Number(event.target.value),
-                    "maxDeliveredThrust"
-                  );
-                }}
-                unit={getUnit(formValues["maxDeliveredThrust"])}
-                units={["kN"]}
-              />
-            </Grid>
-
-            <Grid item xs={12} md={6}>
-              <NumberWithUnitsField
-                label="(σ min) Limite de Cavitação"
-                value={getValue(formValues["cavitationLimit"])}
-                onChange={(event) => {
-                  onChangeValue(Number(event.target.value), "cavitationLimit");
-                }}
-                unit={getUnit(formValues["cavitationLimit"])}
-                units={["%"]}
-              />
-            </Grid>
-          </Grid>
-        </Box>
-      </SectionContainer>
-
-      <SectionContainer>
-        <SectionTitle id={TOC[2].hash}>{TOC[2].text}</SectionTitle>
-        <Box>
-          <Typography>
             Os parâmetros de projeto são as variáveis que serão variadas para
             encontrar diferentes propulsores.
           </Typography>
@@ -384,14 +215,183 @@ function PageContent() {
           </Grid>
         </Box>
       </SectionContainer>
+
+      <SectionContainer>
+        <SectionTitle id={TOC[1].hash}>{TOC[1].text}</SectionTitle>
+        <Box>
+          <Typography>
+            As restrições são as condições que o sistema deve atender para
+            satisfazer os requisitos do projeto.
+          </Typography>
+
+          <Box sx={{ mt: 4 }} />
+
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(n) Máximo Número de Propulsores"
+                value={getValue(formValues["maxNumberOfResults"])}
+                onChange={(event) => {
+                  onChangeValue(
+                    Number(event.target.value),
+                    "maxNumberOfResults"
+                  );
+                }}
+                unit={getUnit(formValues["maxNumberOfResults"])}
+                units={["-"]}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(nr) Eficiência Mínima"
+                value={getValue(formValues["minEfficiency"])}
+                onChange={(event) => {
+                  onChangeValue(Number(event.target.value), "minEfficiency");
+                }}
+                unit={getUnit(formValues["minEfficiency"])}
+                units={["-"]}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(T min) Empuxo Entregue Mínimo"
+                value={getValue(formValues["minDeliveredThrust"])}
+                onChange={(event) => {
+                  onChangeValue(
+                    Number(event.target.value),
+                    "minDeliveredThrust"
+                  );
+                }}
+                unit={getUnit(formValues["minDeliveredThrust"])}
+                units={["kN"]}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(T max) Empuxo Entregue Máximo"
+                value={getValue(formValues["maxDeliveredThrust"])}
+                onChange={(event) => {
+                  onChangeValue(
+                    Number(event.target.value),
+                    "maxDeliveredThrust"
+                  );
+                }}
+                unit={getUnit(formValues["maxDeliveredThrust"])}
+                units={["kN"]}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(σ min) Limite de Cavitação"
+                value={getValue(formValues["cavitationLimit"])}
+                onChange={(event) => {
+                  onChangeValue(Number(event.target.value), "cavitationLimit");
+                }}
+                unit={getUnit(formValues["cavitationLimit"])}
+                units={["%"]}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </SectionContainer>
+
+      <SectionContainer>
+        <SectionTitle id={TOC[2].hash}>{TOC[2].text}</SectionTitle>
+        <Box>
+          <Typography>
+            As variáveis de ambiente configuram as condições físicas nas quais o
+            propulsor deve operar.
+          </Typography>
+
+          <Box sx={{ mt: 4 }} />
+
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(ρ) Densidade da Água do Mar"
+                value={getValue(formValues["seaWaterDensity"])}
+                onChange={(event) => {
+                  onChangeValue(Number(event.target.value), "seaWaterDensity");
+                }}
+                unit={getUnit(formValues["seaWaterDensity"])}
+                units={["kg/m³"]}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(g) Aceleração da Gravidade"
+                value={getValue(formValues["gravityAcceleration"])}
+                onChange={(event) => {
+                  onChangeValue(
+                    Number(event.target.value),
+                    "gravityAcceleration"
+                  );
+                }}
+                unit={getUnit(formValues["gravityAcceleration"])}
+                units={["m/s²"]}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(Pa) Pressão Atmosférica"
+                value={getValue(formValues["atmosphericPressure"])}
+                onChange={(event) => {
+                  onChangeValue(
+                    Number(event.target.value),
+                    "atmosphericPressure"
+                  );
+                }}
+                unit={getUnit(formValues["atmosphericPressure"])}
+                units={["Pa"]}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(Ps) Pressão de Saturação da Água do Mar"
+                value={getValue(formValues["saturationPressure"])}
+                onChange={(event) => {
+                  onChangeValue(
+                    Number(event.target.value),
+                    "saturationPressure"
+                  );
+                }}
+                unit={getUnit(formValues["saturationPressure"])}
+                units={["Pa"]}
+              />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              <NumberWithUnitsField
+                label="(v) Viscosidade Cinemática da Água do Mar"
+                value={getValue(formValues["kinematicViscosity"])}
+                onChange={(event) => {
+                  onChangeValue(
+                    Number(event.target.value),
+                    "kinematicViscosity"
+                  );
+                }}
+                unit={getUnit(formValues["kinematicViscosity"])}
+                units={["m²/s"]}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </SectionContainer>
     </Box>
   );
 }
 
 const TOC: TocItem[] = [
   {
-    text: "Condições Ambientais",
-    hash: "condicoes-ambientais",
+    text: "Parâmetros de Projeto",
+    hash: "parametros-de-projeto",
     children: [],
   },
 
@@ -402,8 +402,8 @@ const TOC: TocItem[] = [
   },
 
   {
-    text: "Parâmetros de Projeto",
-    hash: "parametros-de-projeto",
+    text: "Condições Ambientais",
+    hash: "condicoes-ambientais",
     children: [],
   },
 ];
